ff92bc908de59456880b2965ca79788f1dbd41fb
wpmohammad1 <alimohammadjhd@gmail.com>
2024-07-03 13:07:45 +0600

    shadcn use

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,22 +1,15 @@
+import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
   return (
     <div className="p-4 sm:p-6 md:p-8 lg:p-10">
       <div className="flex justify-between mb-4">
-        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-          Add Todo
-        </button>
+        <AddTodoModal />
         <button className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600">
           Filter
         </button>
       </div>
-      {/* <div>
-        <div className="">
-          <p>There Is no task pending</p>
-          {""}
-        </div>
-      </div> */}
       <div className="bg-red-300 w-full h-auto min-h-[500px] rounded-xl p-4 sm:p-6 md:p-8">
         <table className="w-full">
           <thead>
@@ -34,6 +27,13 @@ const TodoContainer = () => {
             <TodoCard />
             <TodoCard />
           </tbody>
+
+          {/* <div>
+        <div className="">
+          <p>There Is no task pending</p>
+          {""}
+        </div>
+      </div> */}
         </table>
       </div>
     </div>
