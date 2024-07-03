bc074e4db3f60e719562fbc6090bfff3d5658eea
wpmohammad1 <alimohammadjhd@gmail.com>
2024-07-03 11:12:12 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <Button>Click Me</Button>
+    </>
+  );
+}
+
+export default App;
